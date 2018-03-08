a3ffb64ad387d3152492f956c45822cb4feace39
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2018-03-08 17:41:18 -0500

    Finished upto step 32 of chapter 3.

--- kilo.c
+++ kilo.c
@@ -4,33 +4,48 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f) // bitwise-ANDs character with value 00011111
+
+
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+
+
+struct editorConfig E;
 
 /*** terminal ***/
 
 void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
 
 void disableRawMode() {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
 		die("tcsetattr");
 	}
 }
 
 void enableRawMode() { // turns off echo
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
 		die("tcgetattr");
 	}
 	atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); //disables Ctrl-S and Ctrl-Q and fixes Ctrl-M
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8); // Bit mask, sets char size to 8 bits/byte
@@ -43,22 +58,91 @@ void enableRawMode() { // turns off echo
 	}
 }
 
+char editorReadKey() { // Waits for one keypress and returns it
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+int getCursorPosition(int *rows, int *cols) {
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+	printf("\r\n");
+	char c;
+	while (read(STDOUT_FILENO, &c, 1) == 1) {
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+	}
+	editorReadKey();
+	return -1;
+}
+
+int getWindowSize(int *rows, int *cols) {
+	struct winsize ws;
+
+	if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+	    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+	    return getCursorPosition(rows, cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+/*** output ***/
+
+void editorDrawRows() {
+	int y;
+	for (y = 0; y < E.screenrows; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3); // Draws tildes on left
+	}
+}
+
+void editorRefreshScreen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4); // Writing 4 bytes to terminal.  \x1b = ESC
+	write(STDOUT_FILENO, "\x1b[H", 3); // Moves mouse cursor back up.
+
+	editorDrawRows();
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
+/*** input ***/
+
+void editorProcessKeypress() { // Waits for keypress and handles it
+	char c = editorReadKey();
+
+	switch (c) {
+		case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
+		exit(0);
+		break;
+	}
+}
+
 /*** init ***/
+
+void initEditor() {
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+		die("getWindowSize");
+	}
+}
 
 int main() {
 	enableRawMode();
+	initEditor();
 
 	while (1) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-			die("read");
-		}
-		if (iscntrl(c)) {
-	      printf("%d\r\n", c);
-	    } else {
-	      printf("%d ('%c')\r\n", c, c); //carriage return and newline
-	    }
-	    if (c == 'q') break;
+		editorRefreshScreen();
+		editorProcessKeypress();
 	}
 
   return 0;
